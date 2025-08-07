b6805ce1f833eeb73755880360d50637da8a6540
Max Destors <max.destors@leptech.net>
2025-08-07 23:36:05 +0200

    chore: add Laravel 12 support

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,17 +8,11 @@ class TestCase extends \Orchestra\Testbench\TestCase
 {
     public function getEnvironmentSetUp($app)
     {
-        // import the migrations
-        include_once __DIR__.'/../src/database/migrations/2015_07_31_1_email_log.php';
-        include_once __DIR__.'/../src/database/migrations/2016_09_21_001638_add_bcc_column_email_log.php';
-        include_once __DIR__.'/../src/database/migrations/2017_11_10_001638_add_more_mail_columns_email_log.php';
-        include_once __DIR__.'/../src/database/migrations/2018_05_11_115355_use_longtext_for_attachments.php';
+        // import the migration
+        include_once __DIR__.'/../src/Database/Migrations/2023_02_26_001638_create_email_log.php';
 
-        // run the up() method of those migration classes
-        (new \EmailLog)->up();
-        (new \AddBccColumnEmailLog)->up();
-        (new \AddMoreMailColumnsEmailLog)->up();
-        (new \UseLongtextForAttachments)->up();
+        // run the up() method of the migration class
+        (new \CreateEmailLog)->up();
     }
 
     protected function getPackageProviders($app)
